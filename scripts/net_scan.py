b383a44394e37da3a97a08ec14fed5e3205e7183
Michael Astfalk <mtastfalkjr@verizon.net>
2025-01-20 15:43:28 -0500

    Update net_scan.py
    Expanded output of the net_scan.py script.

--- scripts/net_scan.py
+++ scripts/net_scan.py
@@ -7,4 +7,15 @@ port_nums = input("Enter the port range (e.g. 25-50): ")
 
 scan_output = nm.scan(ipaddr, port_nums)
 
-print(scan_output)
+for host in nm.all_hosts():
+    print('---------------------------------')
+    print("Host: {} ({})".format(host, nm[host].hostname()))
+    print("State: {}".format(nm[host].state()))
+
+    for protocol in nm[host].all_protocols():
+        print("Protocol: {}".format(protocol))
+
+        list_port = nm[host][protocol].keys()
+        list_port.sort()
+        for port in list_port:
+            print("Port: {}\tState: {}".format(port, nm[host][proto][port][state]))
