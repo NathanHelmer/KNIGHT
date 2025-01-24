bef536a186ca116918556ae51004a1196aad0e43
Michael Astfalk <mtastfalkjr@verizon.net>
2025-01-24 15:59:24 -0500

    Changed header links to nmap-scans
    Modified links to the nmap-scans page to direct to the django page.

--- djangodir/net_scan/net_scan.py
+++ djangodir/net_scan/net_scan.py
@@ -5,22 +5,20 @@
 
 import nmap
 
-nm = nmap.PortScanner()
+def port_scan(ipaddr, port_nums):
+    nm = nmap.PortScanner()
 
-ipaddr = input("Enter the IP address: ")
-port_nums = input("Enter the port range (e.g. 25-50): ")
+    scan_output = nm.scan(ipaddr, port_nums)
 
-scan_output = nm.scan(ipaddr, port_nums)
+    for host in nm.all_hosts():
+        print('---------------------------------')
+        print("Host: {} ({})".format(host, nm[host].hostname()))
+        print("State: {}".format(nm[host].state()))
 
-for host in nm.all_hosts():
-    print('---------------------------------')
-    print("Host: {} ({})".format(host, nm[host].hostname()))
-    print("State: {}".format(nm[host].state()))
+        for protocol in nm[host].all_protocols():
+            print("Protocol: {}".format(protocol))
 
-    for protocol in nm[host].all_protocols():
-        print("Protocol: {}".format(protocol))
+            list_port = nm[host][protocol].keys()
 
-        list_port = nm[host][protocol].keys()
-
-        for port in list_port:
-            print("Port: {}\tState: {}".format(port, nm[host][protocol][port]['state']))
+            for port in list_port:
+                print("Port: {}\tState: {}".format(port, nm[host][protocol][port]['state']))
