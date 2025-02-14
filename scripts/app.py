fe90fb9e9548afa3a3f44f789c00914244174dab
Matthew Alexander <matthewalexander353@gmail.com>
2025-02-14 18:52:22 -0500

    Updated app.py and nmap-scans.html to connect the backend nmap to the frontend html

--- scripts/app.py
+++ scripts/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, jsonify
-from net_scan import port_scan
+from flask import Flask, render_template, jsonify, request
+from net_scan import port_scan 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../User Interface')
 
 @app.route('/run-nmap/')
-def run_nmap(ipaddr='127.0.0.1', ports='1-100', scan_flags='-sN'):
-    return jsonify(result = port_scan(ipaddr, ports, scan_flags))
+def run_nmap_page():
+    return render_template('nmap-scans.html') 
+
+@app.route('/run-nmap-scan/', methods=['GET'])
+def run_nmap():
+    ipaddr = request.args.get('ip', '192.168.2.241')
+    ports = request.args.get('ports', '1-6000')
+    return jsonify(port_scan(ipaddr, ports, 'sN'))
 
 if __name__ == '__main__':
     app.run(debug=True)
