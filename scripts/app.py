d5f8c3a4c69a248f65e236b57550cf4ee7efedb4
Matthew Alexander <matthewalexander353@gmail.com>
2025-02-14 19:44:08 -0500

    Added navigation functionality through flask

--- scripts/app.py
+++ scripts/app.py
@@ -3,12 +3,24 @@ from net_scan import port_scan
 
 app = Flask(__name__, template_folder='../User Interface')
 
-@app.route('/run-nmap/')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/nmap-scans/')
 def run_nmap_page():
-    return render_template('nmap-scans.html') 
+    return render_template('nmap-scans.html')
+
+@app.route('/nmap-logs/')
+def run_nmap_logs_page():
+    return render_template('nmap-logs.html') 
+
+@app.route('/help/')
+def run_help_page():
+    return render_template('help.html')
 
 @app.route('/run-nmap-scan/', methods=['GET'])
-def run_nmap():
+def run_nmap_scan():
     ipaddr = request.args.get('ip', '192.168.2.241')
     ports = request.args.get('ports', '1-6000')
     return jsonify(port_scan(ipaddr, ports, 'sN'))
