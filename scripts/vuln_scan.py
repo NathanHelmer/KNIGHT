501e73898c29e8dc2e46fa471de438c6f6f9a6f2
Michael Astfalk <mtastfalkjr@verizon.net>
2025-02-20 17:08:22 -0500

    Added the run_vuln_scan function to vuln_scan.py

---
+++ scripts/vuln_scan.py
@@ -0,0 +1,16 @@
+'''
+vuln_scan.py
+Description: Runs an nmap vulnerability scan using a script and returns the results.
+Created 2/20/25
+Updated: 2/20/25
+'''
+
+import nmap
+
+def run_vuln_scan(ipaddr):
+    __script_flag__ = '--script vuln'
+    
+    vs = nmap.PortScanner()
+    vuln_output = vs.scan(ipaddr, '-p-', __script_flag__)
+
+    return vuln_output
