9d420c54acb83b45cedc1c66235b023314f4441e
Michael Astfalk <mtastfalkjr@verizon.net>
2025-02-23 19:27:02 -0500

    Edited vuln-scans.html call to /run-vuln-scan/

--- scripts/vuln_scan.py
+++ scripts/vuln_scan.py
@@ -2,11 +2,14 @@
 vuln_scan.py
 Description: Runs an nmap vulnerability scan using a script and returns the results.
 Created 2/20/25
-Updated: 2/20/25
+Updated: 2/23/25
 '''
 
 import nmap
 
+
+# Precondition: ipaddr is a string of an IP address or IP address range.
+# Postcondition: returns an nmap scan object of scan results on ipaddr.
 def vuln_scanner(ipaddr):
     __script_flag__ = '--script vuln'
     
