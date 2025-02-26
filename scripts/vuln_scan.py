ac129e9163b9c11d30bd260e1b94bf7364c39200
Michael Astfalk <mtastfalkjr@verizon.net>
2025-02-25 22:37:03 -0500

    Update vuln_scan.py

--- scripts/vuln_scan.py
+++ scripts/vuln_scan.py
@@ -14,6 +14,6 @@ def vuln_scanner(ipaddr):
     __script_flag__ = '--script vuln'
     
     vs = nmap.PortScanner()
-    vuln_output = vs.scan(ipaddr, '-p-', __script_flag__)
+    vuln_output = vs.scan(ipaddr, '1-1000', __script_flag__)
 
     return vuln_output
