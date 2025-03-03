a48cf81d6d3ddee051d796b674ff0fc50ba77c95
PluggedCloud542 <matthewalexander353@gmail.com>
2025-03-03 01:28:07 -0500

    added note at the top of osdetection.py

--- scripts/osdetection.py
+++ scripts/osdetection.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+#NOTE: When running the program on linux, run it from ~/PATHTOFLASK --app ~/PATHTOAPP.PY run
+
 def nmap_results_path():
     windowspath = os.path.abspath('scripts\\results\\latest_nmap_results.txt')
     linuxpath = os.path.abspath('./scripts/results/latest_nmap_results.txt')
