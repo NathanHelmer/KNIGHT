c6d4188f9e9411849763fac2a248eea0b9609376
Matthew Alexander <matthewalexander353@gmail.com>
2025-03-03 00:48:30 -0500

    Attemping to fix the linuxpath in osdetection.py

--- scripts/osdetection.py
+++ scripts/osdetection.py
@@ -3,7 +3,8 @@ import os
 
 def nmap_results_path():
     windowspath = os.path.abspath('scripts\\results\\latest_nmap_results.txt')
-    linuxpath = './results/latest_nmap_results.txt'
+    #linuxpath = './results/latest_nmap_results.txt'
+    linuxpath = os.path.abspath('scripts/results/latest_nmap_results.txt')
     if (sys.platform == 'linux'):
         return linuxpath
     else:
