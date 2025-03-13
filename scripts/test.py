d6e5eb592d08a5763c40d0bfb73050c7c91f0e3a
Michael Astfalk <mtastfalkjr@verizon.net>
2025-03-13 13:37:57 -0400

    Created test for vuln scans
    Created the run_vulns() test function in test.py.

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 # Automated testing
 
 from net_scan import *
+from vuln_scan import *
 
 # Precondition: none
 # Postcondition: runs sample scans and ouputs the results to nmap_results.txt
@@ -10,4 +11,11 @@ def run_scans():
 
     port_scan(test_range, test_ports, '-sV')
 
+def run_vulns():
+    test_range = '10.25.1.8'
+    test_ports = '1-1000'
+
+    vuln_scanner(test_range, test_ports)
+
 run_scans()
+run_vulns()
