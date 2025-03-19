f0dbbfb0c30aef0b19f5fa184ebbfadf7afecfee
PluggedCloud542 <matthewalexander353@gmail.com>
2025-03-19 14:31:20 -0400

    osdetection changes

--- scripts/osdetection.py
+++ scripts/osdetection.py
@@ -24,6 +24,7 @@ def nmap_results_path():
     if (sys.platform == 'linux'):
         if not os.path.exists(linuxdir):
             os.makedirs(linuxdir)
+
         return linuxpath
     else:
         if not os.path.exists(windowsdir):
