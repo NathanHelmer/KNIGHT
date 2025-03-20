9c3bc2635f408c71e7a4934f7ae9e22c4c6340fa
Michael Astfalk <mtastfalkjr@verizon.net>
2025-03-19 21:10:51 -0400

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -9,15 +9,17 @@ from net_scan import *
 from vuln_scan import *
 
 # Precondition: none
-# Postcondition: runs sample scans and ouputs the results to nmap_results.txt
+# Postcondition: runs sample nmap scans
 def run_scans():
-    test_range = '127.0.0.1'
+    test_range = 'scanme.nmap.org'
     test_ports = '1-1000'
 
     port_scan(test_range, test_ports, '-sV')
 
+# Precondition: none
+# Postcondition: runs sample vulnerability scans
 def run_vulns():
-    test_range = '10.25.1.8'
+    test_range = 'scanme.nmap.org'
     test_ports = '1-1000'
 
     vuln_scanner(test_range, test_ports)
