65b81ef08dc29393f330bf1d3684731ab647c8d5
Michael Astfalk <mtastfalkjr@verizon.net>
2025-03-24 15:51:49 -0400

    Added output to meta.py and updated comments.

--- scripts/meta.py
+++ scripts/meta.py
@@ -7,8 +7,12 @@ from pymetasploit3.msfrpc import MsfRpcClient
 
 client = MsfRpcClient('NfF9sTEo', port=55553, ssl=True)
 
+# Precondition: cve is a string for the cve name to be searched.
+# Postcondition: returns a the name/path of the exploit module if found.
 def search_exploit(cve):
-    print(client)
+    for m in dir(client):
+        if not m.startswith('_'):
+            print(m)
     return
 
 search_exploit('test')
