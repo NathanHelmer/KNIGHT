65e4bb0c578d0be972eca0192e4ade9ac53d3c90
Michael Astfalk <mtastfalkjr@verizon.net>
2025-03-28 16:43:52 -0400

    Updated meta.py

--- scripts/meta.py
+++ scripts/meta.py
@@ -15,28 +15,22 @@ server on port 55553. The current password to being the service is NfF9sTEo. The
 will connect to the server on port 55553 and return a metasploit object ot client.
 '''
 
-try:
-    client = MsfRpcClient('NfF9sTEo', port=55553, ssl=True)
-except:
-    print("Failed to connect to MsfRpcClient")
-    exit(0)
+__password__ = 'NfF9sTEo'
 
 try:
-    console = MsfRpcConsole(client, cb=read_console)
+    client = MsfRpcClient(__password__, port=55553, ssl=True)
 except:
-    print("Failed to initialize console")
+    print("Failed to connect to MsfRpcClient")
     exit(0)
 
 # Precondition: cve is a string for the cve name to be searched.
 # Postcondition: returns a the name/path of the exploit module if found.
 def search_exploit(cve):
-    for m in dir(client):
-        if not m.startswith('_'):
-            print(m)
-            if m == 'modules':
-                print('\nmodule:\n')
-                for i in dir(m):
-                    print(i)
+    for m in client.modules.exploits:
+        exploit = client.modules.use('exploit', m)
+
+        if cve in exploit.description:
+            return exploit.description
 
     return
 
