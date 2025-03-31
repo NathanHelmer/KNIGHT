ee6a8dd790ccf0c0c7e6294388d00fe919556755
Admin KNIGHT <admin@kali-raspberrypi>
2025-03-31 14:37:52 -0400

    Updated meta.py script

--- scripts/meta.py
+++ scripts/meta.py
@@ -9,15 +9,21 @@ from pymetasploit3.msfrpc import MsfRpcClient
 
 '''
 Instructions for MsfRpcClient:
+Load msfrpc in msf console using the `load msgprc` command.
+
 The RPC server can be started using the command `msfrpcd -P <password>`. This command will start the
 server on port 55553. The current password to being the service is NfF9sTEo. The MsfRpcClient function
 will connect to the server on port 55553 and return a metasploit object ot client.
 '''
 
-__password__ = 'NfF9sTEo'
+__password__ = 'nKJyB2N5'
+
+print('Connecting to MsfRpcClient...')
 
 try:
-    client = MsfRpcClient(__password__, port=55553, ssl=True)
+    print('Trying connection...')
+    client = MsfRpcClient(__password__, port=55552, ssl=True)
+    print('Connected to MsfRpcClient')
 except:
     print("Failed to connect to MsfRpcClient")
     exit(0)
@@ -25,6 +31,9 @@ except:
 # Precondition: cve is a string for the cve name to be searched.
 # Postcondition: returns a the name/path of the exploit module if found.
 def search_exploit(cve):
+
+    print('Searching for exploits...')
+
     for m in client.modules.exploits:
         exploit = client.modules.use('exploit', m)
 
