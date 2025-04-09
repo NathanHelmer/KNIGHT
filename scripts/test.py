63eb3d0fb7734972571f6dd1d6a903a8a94c189b
Michael Astfalk <mtastfalkjr@verizon.net>
2025-04-09 14:17:29 -0400

    Added test_meta() function to test meta.py

--- scripts/test.py
+++ scripts/test.py
@@ -7,6 +7,7 @@ Updated: 3/18/25
 
 from net_scan import *
 from vuln_scan import *
+from meta import *
 
 # Precondition: none
 # Postcondition: runs sample nmap scans
@@ -23,3 +24,13 @@ def run_vulns():
     test_ports = '1-1000'
 
     vuln_scanner(test_range, test_ports)
+
+# Precondition: none
+# Postocndition: runs a sample search of CVEs from 2017 and outputs their names to the terminal
+def test_meta():
+    search_term = 'CVE-2017'
+
+    findings = search_exploit(search_term)
+
+    for i in findings:
+        print("Found exploit: ", i.name, "\n")
